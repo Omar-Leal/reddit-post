426de734128505459434e11f59fd56e5c97b0c07
Omar Leal <omarenriquelealr@gmail.com>
2024-04-05 12:24:13 -0400

    setting post table view cell

---
+++ Reddit Posts/CustomCell/PostViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostViewCell.swift
+//  Reddit Posts
+//
+//  Created by Omar Leal on 4/5/24.
+//
+
+import UIKit
+
+class PostViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Reddit Posts/ViewController.swift
+++ Reddit Posts/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var allRedditPost: [RedditPostData] = []
 
+    @IBOutlet weak var postTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        fetchAllRedditPosts { (result, error)  in
+            if let alldata = result {
+                self.allRedditPost = alldata
+                self.postTableView.reloadData()
+            }
+            
+        }
     }
 
 
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        allRedditPost.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
 }
 
